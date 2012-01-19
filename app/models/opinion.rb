740eb62a5728552ecbb01eabf1c0e3834cffb89d
Victor Costan <costan@gmail.com>
2012-01-19 05:19:52 -0500

    Opinion model: added name uniqueness and format validations.

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,3 +1,7 @@
 class Opinion < ActiveRecord::Base
   belongs_to :issue
+  
+  validates :name, uniqueness: { scope: [:issue_id],
+      message: 'You can only vote once on each issue' },
+      format: { with: /^[A-Za-z ]+$/ }
 end
