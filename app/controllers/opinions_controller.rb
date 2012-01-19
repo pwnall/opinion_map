aab4a9181c2194037c1e68672186dbd18f63981f
Victor Costan <costan@gmail.com>
2012-01-19 04:53:37 -0500

    Opinions: removed edit, show, and update views and controller logic.

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -10,17 +10,6 @@ class OpinionsController < ApplicationController
     end
   end
 
-  # GET /opinions/1
-  # GET /opinions/1.json
-  def show
-    @opinion = Opinion.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @opinion }
-    end
-  end
-
   # GET /opinions/new
   # GET /opinions/new.json
   def new
@@ -30,11 +19,6 @@ class OpinionsController < ApplicationController
       format.html # new.html.erb
       format.json { render json: @opinion }
     end
-  end
-
-  # GET /opinions/1/edit
-  def edit
-    @opinion = Opinion.find(params[:id])
   end
 
   # POST /opinions
@@ -48,22 +32,6 @@ class OpinionsController < ApplicationController
         format.json { render json: @opinion, status: :created, location: @opinion }
       else
         format.html { render action: "new" }
-        format.json { render json: @opinion.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /opinions/1
-  # PUT /opinions/1.json
-  def update
-    @opinion = Opinion.find(params[:id])
-
-    respond_to do |format|
-      if @opinion.update_attributes(params[:opinion])
-        format.html { redirect_to @opinion, notice: 'Opinion was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
         format.json { render json: @opinion.errors, status: :unprocessable_entity }
       end
     end
