d80d4f7d288eb72e6bb005549fa5f4a5127ab8d8
Victor Costan <costan@gmail.com>
2012-01-19 02:19:12 -0500

    rails new opinion_map

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpinionMap::Application.config.secret_token = 'cecb91aa41bab5d0eca825c4319d0ccd6d3aec522dd98c347e612e6d19b07680b6964b7cda1a55c8fc8e84fe4608ed8dd0cd8204e573bf24fc0d96c357cb1390'
