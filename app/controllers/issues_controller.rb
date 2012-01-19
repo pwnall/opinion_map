79370e944fcff5a133db72367e738dd50e9f85c0
Victor Costan <costan@gmail.com>
2012-01-19 02:48:13 -0500

    Issues controller: removed show, edit, destroy. Replaced update's name and headers with those of destroy.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -10,17 +10,6 @@ class IssuesController < ApplicationController
     end
   end
 
-  # GET /issues/1
-  # GET /issues/1.json
-  def show
-    @issue = Issue.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @issue }
-    end
-  end
-
   # GET /issues/new
   # GET /issues/new.json
   def new
@@ -30,11 +19,6 @@ class IssuesController < ApplicationController
       format.html # new.html.erb
       format.json { render json: @issue }
     end
-  end
-
-  # GET /issues/1/edit
-  def edit
-    @issue = Issue.find(params[:id])
   end
 
   # POST /issues
@@ -53,31 +37,19 @@ class IssuesController < ApplicationController
     end
   end
 
-  # PUT /issues/1
-  # PUT /issues/1.json
-  def update
-    @issue = Issue.find(params[:id])
-
-    respond_to do |format|
-      if @issue.update_attributes(params[:issue])
-        format.html { redirect_to @issue, notice: 'Issue was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @issue.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /issues/1
   # DELETE /issues/1.json
   def destroy
     @issue = Issue.find(params[:id])
-    @issue.destroy
 
     respond_to do |format|
-      format.html { redirect_to issues_url }
-      format.json { head :no_content }
+      if @issue.update_attributes closed: true
+        format.html { redirect_to issues_url, notice: 'Issue was successfully closed.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "index" }
+        format.json { render json: @issue.errors, status: :unprocessable_entity }
+      end
     end
   end
 end
